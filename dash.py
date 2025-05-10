0ae918e86d81516bcd0cf5a2c705151529e4aacf
Almirzitoz <abner.fermino111@gmail.com>
2025-05-10 16:12:53 -0700

    filtro de tipagem

--- dash.py
+++ dash.py
@@ -3,3 +3,7 @@ import pandas as pd
 import streamlit as st 
 
 df = pd.read_csv("pokemon.csv")
+
+grass = df[df['type1'].str.contains("grass",case=False,na=False)|
+           df['type2'].str.contains("grass",case=False,na=False)]
+
