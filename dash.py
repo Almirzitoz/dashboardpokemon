a8bbc88ddd9219bdf730f107d30993fa8d188d1f
Almirzitoz <abner.fermino111@gmail.com>
2025-05-10 19:03:46 -0700

    titulos

--- dash.py
+++ dash.py
@@ -18,15 +18,19 @@ def main():
     col1,col2 =st.columns(2)
     col3,col4 = st.columns(2)
     with col1:
+        st.title("Media dos status base por geração")
         grafico_linha = st.line_chart(data=media_status)
     with col2:
+     st.title("Media dos status base por geração")
      st.scatter_chart(data=media_status)
     with col3:
+     st.title("Media do status total por geração")
      grafico_colula =st.bar_chart(data= media_total)
      
     tipos_comuns = pd.concat([tipos_type1, tipos_type2]).value_counts()
     
     with col4:
+        st.title("Tipos mais comuns em todas as gerações")
         fig, grafico = plt.subplots()
         grafico.pie(tipos_comuns, labels=tipos_comuns.index, autopct='%1.1f%%', startangle=90)
         grafico.axis('equal')
